d37d958d48c37983c1c377c6e15a54536d963d32
augbor2000 <augustinborozan2000@gmail.com>
2025-04-12 23:34:51 +0300

    Static variable and Static methods + class vs record

---
+++ RecordProject/src/Main.java
@@ -0,0 +1,34 @@
+
+public class Main {
+    public static void main(String[] args) {
+
+        for(int i = 1; i <= 5; i++){
+            LPAStudent s = new LPAStudent("s92300",
+                    switch(i){
+                        case 1-> "Mary";
+                        case 2-> "John";
+                        case 3-> "Jane";
+                        case 4-> "Bob";
+                        case 5-> "Lisa";
+                        default -> "Anonymous";
+            },"05/11/1985","Java Masterclass");
+
+            System.out.println(s);
+
+
+            Student pojoStudent = new Student("s923006","Anna","08/12/1985","Java Masterclass");
+            LPAStudent recordStudent = new LPAStudent("s923007","Bill","12/08/1985","Java Masterclass");
+
+            System.out.println(pojoStudent);
+            System.out.println(recordStudent);
+
+            pojoStudent.setClassList(pojoStudent.getClassList() + ", Java OCP Exam 829");
+          //  recordStudent.setClassList(recordStudent.classList() + ", Java OCP Exam 829");
+
+            System.out.println(pojoStudent.getName() + "is taking " + pojoStudent.getClassList());
+            System.out.println(recordStudent.name() + "is taking " + recordStudent.classList());
+
+
+        }
+    }
+}
