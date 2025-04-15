000c43a5a1312c6dee75e0097f343c8328cd5fa6
augbor2000 <augustinborozan2000@gmail.com>
2025-04-15 23:11:29 +0300

    Introduction to Inheritance

---
+++ Masterclass/src/OOP_Section7/Dog.java
@@ -0,0 +1,8 @@
+package OOP_Section7;
+
+public class Dog extends Animal {
+
+    public Dog() {
+        super("Mutt","Big",50);
+    }
+}
---
+++ Masterclass/src/OOP_Section7/Inheritance.java
@@ -0,0 +1,21 @@
+package OOP_Section7;
+
+public class Inheritance {
+    public static void main(String[] args) {
+
+
+        Animal animal = new Animal("Generic Animal","Huge",400);
+        doAnimalStuff(animal,"slow");
+
+        Dog dog = new Dog();
+        doAnimalStuff(dog,"fast");
+    }
+    public static void doAnimalStuff(Animal animal,String speed){
+        animal.makeNoise();
+        animal.move(speed);
+        System.out.println(animal);
+        System.out.println("____________");
+    }
+
+
+}
